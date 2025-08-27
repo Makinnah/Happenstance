39c1b9958f09740fa7edebdc37016e97d5b7d76f
Duncan Makinnah Osuka <makinnahdavid@gmail.com>
2025-08-27 13:11:43 +0300

    Update settings.py

--- Happenstance/settings.py
+++ Happenstance/settings.py
@@ -74,8 +74,15 @@ WSGI_APPLICATION = 'Happenstance.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'happenstance_db',
+        'USER': 'makinnah_user',
+        'PASSWORD': 'Password2025#!',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
